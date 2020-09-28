40ec1f9c02f8508eebe49a3a6904738edcec093f
GianlucaVico <ganlucavico@gmail.com>
2020-09-28 19:42:52 +0200

    Implemented the method to generate tetrahedron and fix some problems in the method to create the nodes

--- SiliconeSheet.py
+++ SiliconeSheet.py
@@ -27,7 +27,7 @@ class Sheet:
         self,
         pos: Tuple[float, float, float],
         size: Tuple[float, float, float],
-        subdiv: Union[float, Tuple[float, float, float]],
+        subdiv: Union[int, Tuple[int, int, int]],
         mass: float = 0,
     ) -> None:
 
@@ -42,6 +42,7 @@ class Sheet:
             subdiv = (subdiv,) * 3
 
         self._make_nodes(pos, size, subdiv, mass)
+        self._make_elements()
 
     @property
     def mesh(self) -> fea.ChMesh:
@@ -80,11 +81,26 @@ class Sheet:
         # TODO find a better way to identify faces and edges
         pass
 
+    def set_visualization(self, *args) -> None:
+        """
+        Set how the mesh will be displayed in the simulation.
+
+        Multiple calls to this methods does not overwrite the previous visualization, but
+        but displays all the visualizations set
+
+        Args:
+
+        Returns:
+            None
+
+        """
+        # TODO
+
     def _make_nodes(
         self,
         pos: Tuple[float, float, float],
         size: Tuple[float, float, float],
-        subdiv: Tuple[float, float, float],
+        subdiv: Tuple[int, int, int],
         mass,
     ) -> None:
         """
@@ -100,9 +116,7 @@ class Sheet:
         mass /= (subdiv[0] + 1) * (subdiv[1] + 1) * (subdiv[2] + 1)
         # for each dimension generate the list of positions where to generate the nodes
         # the index of the node identify the posiion in the grid
-        grid = np.ndarray(
-            [np.linspace(0, dim, sub + 1) for dim, sub in zip(size, subdiv)]
-        )
+        grid = [np.linspace(0, dim, sub + 1) for dim, sub in zip(size, subdiv)]
 
         # 3d array containing the nodes
         self._nodes = np.ndarray(
@@ -111,12 +125,12 @@ class Sheet:
 
         # iterator for self._nodes
         with np.nditer(
-            self._nodes, flags=["multi_index"], op_flags=["readwrite"]
+            self._nodes, flags=["multi_index", "refs_ok"], op_flags=["readwrite"]
         ) as it:
             for i in it:
                 ind = it.multi_index  # index of the current node
                 node_pos = chrono.ChVectorD(
-                    grid[ind[0]][ind[1]][ind[2]]
+                    grid[0][ind[0]], grid[1][ind[1]], grid[2][ind[2]]
                 )  # local position of the node
                 tmp = fea.ChNodeFEAxyz(pos + node_pos)
                 tmp.SetMass(mass)
@@ -127,9 +141,84 @@ class Sheet:
         """
         Generate the tetrahedrons from the list of nodes
 
+        Idea:
+            * find a "cube"
+            * generate 5 fea.ChElementTetra_4
+            * repeat until it traversed the entire grid
 
+            Nodes in the cube:
+
+              8-----7
+             /|    /|
+            5-----6 |
+            | 4---|-3
+            |/    |/
+            1-----2
+
+            7 has index (i, j, k), 1 has (i-1, j-1, k-1)
         Returns:
             None.
 
         """
-        pass
+        # number of nodes per dimension
+        x, y, z = self._nodes.shape
+
+        # iterators for x, y, z
+        i = j = k = 1
+
+        e = 0  # index of the element being created -> easier than retrieving it with i,j,k
+        self._elements = [None] * (x * y * z * 5)
+
+        # TODO find a better way to represent the cube
+        n1 = n2 = n3 = n4 = n5 = n6 = n7 = n8 = None
+
+        while i < x:
+            while j < y:
+                while k < z:
+                    # update cube
+                    n1 = self._nodes[i - 1, j - 1, k - 1]
+                    n2 = self._nodes[i, j - 1, k - 1]
+                    n3 = self._nodes[i, j, k - 1]
+                    n4 = self._nodes[i - 1, j, k - 1]
+                    n5 = self._nodes[i - 1, j - 1, k]
+                    n6 = self._nodes[i, j - 1, k]
+                    n7 = self._nodes[i, j, k]
+                    n8 = self._nodes[i - 1, j, k]
+
+                    # tetra 1
+                    tmp = self._make_tetra(n1, n2, n3, n6)
+                    self._elements[e] = tmp
+                    e += 1
+
+                    # tetra 2
+                    tmp = self._make_tetra(n1, n3, n4, n8)
+                    self._elements[e] = tmp
+                    e += 1
+
+                    # tetra 3
+                    tmp = self._make_tetra(n1, n3, n6, n8)
+                    self._elements[e] = tmp
+                    e += 1
+
+                    # tetra 4
+                    tmp = self._make_tetra(n1, n5, n6, n8)
+                    self._elements[e] = tmp
+                    e += 1
+
+                    # tetra 5
+                    tmp = self._make_tetra(n3, n6, n7, n8)
+                    self._elements[e] = tmp
+                    e += 1
+
+                    k += 1
+                j += 1
+            i += 1
+
+    def _make_tetra(self, nodeA, nodeB, nodeC, nodeD):
+        """
+        Make single tetrahedron
+        """
+        tetra = fea.ChElementTetra_4()
+        self._mesh.AddElement(tetra)
+        tetra.SetNodes(nodeA, nodeB, nodeC, nodeD)
+        return tetra
